33cd772c64570ec54068f3dc3624d27b5f514129
Wonkario <46362931+Wonkario@users.noreply.github.com>
2019-06-13 23:12:10 -0500

    Se agregan íconos de Font Awesome y Bootstrap (#1)

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Magikamente',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -23,19 +23,23 @@ export default {
   ** Global CSS
   */
   css: [
+    '~/assets/css/fonts.css',
+    'bootstrap/dist/css/bootstrap.css',
+    'bootstrap-vue/dist/bootstrap-vue.css',
+    '@fortawesome/fontawesome-svg-core/styles.css',
+    '~/assets/css/default.css'
   ],
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/fontawesome.js'
   ],
   /*
   ** Nuxt.js modules
   */
   modules: [
-    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma',
-    '@nuxtjs/pwa',
+    
   ],
   /*
   ** Build configuration
